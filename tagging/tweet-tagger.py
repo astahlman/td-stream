2cfade1b7f4c938f52effb016f7303eeb5056677
Andrew Stahlman <andrew.stahlman@gmail.com>
2015-02-22 22:36:54 -0800

    Player is correctly detected for the sample data using the bigram construction approach - still need to determine whether this will generalize.

--- tagging/tweet-tagger.py
+++ tagging/tweet-tagger.py
@@ -1,55 +1,45 @@
 import nltk
-from collections import Counter
+import operator
 import itertools
+import re
+from collections import Counter
 
 nltk.data.path.append('/Users/astahlman/Documents/Programming/ML/td-stream/nltk')
 
 tweets = [line.strip() for line in open('pos-data.txt')]
-# tweets = ["Touchdown Murray!",
-# "TD",
-# "Touchdown DeMarco Murray!!!!!",
-# "TD #SprayTan #DALvsPHI",
-# "TD Murray!!!!!!",
-# "TOUCHDOWN",
-# "Touchdown DALLAS!",
-# "Touchdown !!",
-# "Touchdown!",
-# "We dem boyzz!!!! #TouchDown #EatADickPhilly",
-# "TOUCHDOWN!!!",
-# "TD hoe",
-# "RT @FishPondTAMU: Touchdown Cowboys!",
-# "Cowboys touchdown",
-# "TOUCHDOWN",
-# "TOUCHDOWN @dallascowboys",
-# "Touchdown!",
-# "Touchdown.",
-# "Touchdown \ud83d\udcaf\ud83d\udc4d",
-# "TOUCHDOWN!!!!",
-# "Ya touchdown",
-# "Touchdown bitch !",
-# "\ud83d\ude4c touchdown",
-# "TD!!!",
-# "Touchdown #CowboysNation",
-# "TOUCHDOWN",
-# "Touchdown",
-# "TOUCHDOWN!!!!",
-# "TD #SprayTan \u2b50\ufe0f\ud83d\udc99\ud83c\udfc8 !!!!!!",
-# "Touchdown",
-# "TOUCHDOWN BABY",
-# "Touchdown #SprayTan",
-# "touchdown! @DeMarcoMurray #DALvsPHI",
-# "TOUCHDOWN HOES",
-# "COWBOYS TOUCHDOWN \ud83d\ude4c\ud83d\ude4c\ud83d\ude4c",
-# "TD\ud83d\udc99",
-# "Touchdown babyyyyyyyy",
-# "TD DeMarco!",
-# "TOUCHDOWN BABYYYY \ud83d\udc99",
-# "Touchdown!",
-# "TOUCHDOWN DEMARCO MURRAY!! #DALvsPHI"]
 
+STOP_WORDS = set(["td", "touchdown", "rt", "baby"]).union(nltk.corpus.stopwords.words('english'))
+PLAYERS = set([line.strip() for line in open('players.txt')])
+NUM_BIGRAMS = 25
 
-all_tokens = [nltk.word_tokenize(tweet) for tweet in tweets]
-all_tags = [nltk.pos_tag(tokens) for tokens in all_tokens]
-flat_tags = [tag for tag_list in all_tags for tag in tag_list]
-proper_nouns = [tag[0].lower() for tag in flat_tags if (tag[1] == 'NNP' and tag[0].lower() not in ["touchdown", "td"])]
-ordered = collections.OrderedDict(sorted(Counter(proper_nouns).items(), key=lambda kv: kv[1], reverse=True))
+def normalize(word):
+    return re.sub("['-.]", "", word.lower())
+
+def bigrams(tweet):
+    words = nltk.word_tokenize(tweet)
+    words = [normalize(word) for word in words]
+    words = [word for word in words if re.match('^[\w-]+$', word) is not None]
+    words = [word for word in words if word not in STOP_WORDS]
+    words = [x for x in words if x]
+    nextword = iter(words)
+    next(nextword, None)
+    bigram = itertools.izip(words,nextword)
+    return list(bigram)
+
+def find_player(bigrams):
+    candidates = set([p1 + " " + p2 for (c, (p1, p2)) in bigrams])
+    matches = candidates.intersection(PLAYERS)
+    if len(matches) > 1:
+        raise "More than one potential match: %s" % list(matches)
+    elif not matches:
+        return None
+    else:
+        return matches.pop()
+
+all_bis = [bigrams(t) for t in tweets]
+all_bis = [x for x in all_bis if x]
+all_bis = [x for sublist in all_bis for x in sublist]
+sorted_bis = sorted(((c, b) for b,c in Counter(all_bis).iteritems()), reverse=True)
+
+scorer = find_player(sorted_bis[:NUM_BIGRAMS]) or "Someone"
+print scorer + " scored a touchdown! Hooray!"
